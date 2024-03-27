d6125351a171a276d1e330466ab0b9eefa005f5c
TripelThreat <102275109+TripelThreat@users.noreply.github.com>
2024-03-27 21:05:19 +0700

    cm

--- Sample1_MyAttempt/StackSimulation.java
+++ Sample1_MyAttempt/StackSimulation.java
@@ -1,3 +1,5 @@
+package Sample1_myattempt;
+
 public class StackSimulation {
     private String[] stackArray;
 
@@ -5,24 +7,24 @@ public class StackSimulation {
         this.stackArray = stack;
     }
 
-    public String[] popAll(String[] stack){
+    public String[] popAll(String[] stack) {
         String[] returnPop = new String[stack.length];
-        for (int i = 0; i < stack.length; i++){
+        for (int i = 0; i < stack.length; i++) {
             returnPop[i] = stack[stack.length - i - 1];
         }
         return returnPop;
-    }    
+    }
 
     public static void main(String[] args) {
-        String[] letter = {"A", "B", "C", "D"};
+        String[] letter = { "A", "B", "C", "D" };
         StackSimulation stack = new StackSimulation(letter);
-        
+
         // Call popAll method to get the reversed array
         String[] reversedArray = stack.popAll(letter);
-        
+
         // Print each element of the reversed array
         for (String str : reversedArray) {
             System.out.print(str + "");
         }
-    }    
+    }
 }
